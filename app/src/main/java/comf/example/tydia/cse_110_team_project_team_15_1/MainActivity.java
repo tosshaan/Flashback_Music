8204de785409f29f8bc2b4222bc4a7bdb498f30f
turf <tydia@me.com>
2018-02-06 16:59:29 -0800

    switch modes

--- app/src/main/java/comf/example/tydia/cse_110_team_project_team_15_1/MainActivity.java
+++ app/src/main/java/comf/example/tydia/cse_110_team_project_team_15_1/MainActivity.java
@@ -2,6 +2,9 @@ package comf.example.tydia.cse_110_team_project_team_15_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button launchFlashbackActivity = (Button) findViewById(R.id.b_flashback);
+
+        launchFlashbackActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchFlashback();
+            }
+        });
+    }
+
+    public void launchFlashback() {
+        Intent intent = new Intent (this, FlashbackActivity.class);
+        startActivity(intent);
     }
 }
