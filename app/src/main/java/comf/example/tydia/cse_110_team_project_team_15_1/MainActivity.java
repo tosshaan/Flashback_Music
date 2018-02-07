59ce6dcf51e2ad148df2d109c7babaff0e27c83b
turf <tydia@me.com>
2018-02-06 16:29:15 -0800

    empty project

---
+++ app/src/main/java/comf/example/tydia/cse_110_team_project_team_15_1/MainActivity.java
@@ -0,0 +1,13 @@
+package comf.example.tydia.cse_110_team_project_team_15_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
