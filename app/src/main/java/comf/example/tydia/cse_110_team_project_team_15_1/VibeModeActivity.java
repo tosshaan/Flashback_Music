1572c08cedf9833da36d5451611ddacb15d61326
turf <tydia@me.com>
2018-03-14 21:26:53 -0700

    vibe mode working version

--- app/src/main/java/comf/example/tydia/cse_110_team_project_team_15_1/VibeModeActivity.java
+++ app/src/main/java/comf/example/tydia/cse_110_team_project_team_15_1/VibeModeActivity.java
@@ -60,15 +60,6 @@ public class VibeModeActivity extends AppCompatActivity implements Observer {
         musicPlayer.regObserver(this);
         downloadManager.regObserver(musicPlayer);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
 
         Location myLoc = MainActivity.getCurrLoc();
         String currAddress = "";
